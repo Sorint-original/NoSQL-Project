ffde669e30e7802c19cc31379a4141895e1e1f89
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-10-02 11:45:10 +0200

    lib references

--- NOSQL-Project/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ NOSQL-Project/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8338ef2c205bf0167bff2d0996f8f47b753bfa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3816cecc0f2021472a8b942d9420d09e9a786300")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
