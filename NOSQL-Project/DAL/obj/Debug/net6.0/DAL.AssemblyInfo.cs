c2b16a68f38eedbfc333382e4e2d298058e5e978
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-10-02 12:30:36 +0200

    Base classes

--- NOSQL-Project/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ NOSQL-Project/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3816cecc0f2021472a8b942d9420d09e9a786300")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffde669e30e7802c19cc31379a4141895e1e1f89")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ NOSQL-Project/Model/Ticket.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Ticket
+    {
+        [BsonElement("_id")]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Employee_id")]
+        public ObjectId EmployeeId { get; set; }
+
+        [BsonElement("Title")]
+        public string Title { get; set; }
+
+        [BsonElement("Description")]
+        public string Description { get; set; }
+
+        [BsonElement("Status")]
+        [BsonRepresentation(BsonType.String)]
+        public Status Status { get; set; }
+
+        [BsonElement("Creation_Time")]
+        public DateTime CreationTime { get; set; }
+
+        [BsonElement("Solution_Time")]
+        public DateTime SolutionTime { get; set; }
+
+        public Ticket(ObjectId id, ObjectId Employeeid, string Title, string Description, Status Status, DateTime CreationTime, DateTime SolutionTime)
+        {
+            this.Id = id;
+            this.EmployeeId = Employeeid;
+            this.Title = Title;
+            this.Description = Description;
+            this.Status = Status;
+            this.CreationTime = CreationTime;
+            this.SolutionTime = SolutionTime;
+        }
+    }
+}
+
+
+enum Status
+{
+    open,pending,closed
+}
