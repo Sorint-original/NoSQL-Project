1b6e6531231f4d7d4caa47103464b056f11a9f35
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-10-06 15:37:56 +0200

    basedao fix

--- No-sql Project/DAL/BaseDAO.cs
+++ No-sql Project/DAL/BaseDAO.cs
@@ -14,12 +14,24 @@ namespace DAL
     public class BaseDAO
     {
         private MongoClient client;
-      
+        protected IMongoDatabase database;
+
         public BaseDAO()
         {
             client = new MongoClient(ConfigurationManager.ConnectionStrings["mongoconnectionstring"].ConnectionString);
 
+            //connect to a specific database
+
+            database = client.GetDatabase("ManagementSystem");
+
         }
+
+        //method to allow the ticket and employee classes  to access collection
+        protected IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return database.GetCollection<T>(collectionName);
+        }
+
 
     }
 }
