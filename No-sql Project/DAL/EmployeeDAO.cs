a154259db46774fe20c20be7d2e4935c3a41114f
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-10-06 15:27:17 +0200

    quck references

--- No-sql Project/DAL/EmployeeDAO.cs
+++ No-sql Project/DAL/EmployeeDAO.cs
@@ -1,4 +1,12 @@
-﻿namespace DAL
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+namespace DAL
 {
     public class EmployeeDAO: BaseDAO
     {
--- No-sql Project/Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
+++ No-sql Project/Service/obj/Debug/net6.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ea8b3774a5a01049e884d1b3f1357f92b89009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0952ace5c59b1d7340543f30cb6228898cda091f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
