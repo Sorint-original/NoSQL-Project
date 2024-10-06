f1a0e7802a13dec24fde913193db9471cf65b947
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2024-10-06 15:44:55 +0200

    Leith part

--- No-sql Project/DAL/TicketDAO.cs
+++ No-sql Project/DAL/TicketDAO.cs
@@ -4,10 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace DAL
 {
     internal class TicketDAO : BaseDAO
     {
+        private readonly IMongoCollection<Ticket> _ticketsCollection;
+
+        public TicketDAO() : base()
+        {
+            _ticketsCollection = GetCollection<Ticket>("Tickets");
+        }
+
+        //GetAllTickets(order by Status)
+        public List<Ticket> GetAllTickets()
+        {
+            var sort = Builders<Ticket>.Sort.Ascending(t => t.Status);
+            return _ticketsCollection.Find(FilterDefinition<Ticket>.Empty).Sort(sort).ToList();
+        }
+
+        //GetTiketsByEmployeeId
+        public List<Ticket> GetTicketsByEmployeeId(ObjectId employeeId)
+        {
+            var filter = Builders<Ticket>.Filter.Eq(t => t.EmployeeId, employeeId);
+            var sort = Builders<Ticket>.Sort.Ascending(t => t.Status);
+            return _ticketsCollection.Find(filter).Sort(sort).ToList();
+        }
+
+        //create a new ticket
+        public void CreateTicket(ObjectId employeeId, string title, string description, Priority priority)
+        {
+            var newTicket = new Ticket(
+                ObjectId.GenerateNewId(),   // Generate a new ObjectId
+                employeeId,
+                title,
+                description,
+                Status.open,
+                priority,
+                DateTime.UtcNow,
+                DateTime.MinValue            // Set SolutionTime to MinValue as it's not resolved yet
+            );
+
+            _ticketsCollection.InsertOne(newTicket);
+            Console.WriteLine("Ticket created successfully.");
+        }
+
+        //Update tickets
+
+        //Delete tickets
+
+        //GetAlltickets by status
+
     }
 }
