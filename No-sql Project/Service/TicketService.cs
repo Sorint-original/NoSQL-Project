4c5351c7d27f452ba9e9392ced4884ce8b3e64cc
lai67 <144331843+lai67@users.noreply.github.com>
2024-10-11 23:07:10 +0200

    update laith part
    i have created a class for my individual assignment which is Filtering .

--- No-sql Project/Service/TicketService.cs
+++ No-sql Project/Service/TicketService.cs
@@ -1,7 +1,28 @@
-﻿namespace Service
+﻿using System.Collections.Generic;
+using DAL;
+using Model;
+namespace Service
 {
     public class TicketService
     {
+        private TicketDAO ticketDAO;
+        private TicketFiltering ticketFiltering;
+        private List<Ticket> preloadedTickets; // load the tickt form the database once and reuse them when needed
 
+        public TicketService()
+        {
+            ticketDAO = new TicketDAO();
+            ticketFiltering = new TicketFiltering();
+            preloadedTickets = ticketDAO.GetAllTickets();
+        }
+         public List<Ticket> GetAllTickets()
+        {
+            return preloadedTickets; //  Return the preloaded tickets instead of querying the database again
+        }
+
+        public List<Ticket> FilterTickets(string keyword)
+        {
+            return ticketFiltering.Filtertickets(preloadedTickets, keyword);
+        }
     }
 }
